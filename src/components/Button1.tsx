ffa86566bb15540cd6c28384edd3ee18ad3501cd
jsh0116 <seunghoon7561@gmail.com>
2023-03-19 16:29:21 +0900

    postcss 실습
    - 중복 classname으로 인해 스타일이 덮어쓰기 되는 오류가 발생할 수 있음
    - css 적용을 모듈화하여 적용 (postcss)

---
+++ src/components/Button1.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../css/Button1.module.css';
+
+const Button1 = () => {
+  return <button className={styles.Button}>Button1</button>;
+};
+
+export default Button1;
---
+++ src/components/Button2.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../css/Button2.module.css';
+
+const Button2 = () => {
+  return <button className={styles.Button}>Button2</button>;
+};
+
+export default Button2;
